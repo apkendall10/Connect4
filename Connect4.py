942b763cf928b45f7f115c412b708eb6d2a9c367
Amos Kendall <apkendall10@gmail.com>
2019-09-23 15:43:01 -0500

    Updates

--- Connect4.py
+++ Connect4.py
@@ -4,7 +4,6 @@ size = width, height = 600, 480
 white= 255,255,255
 screen = pygame.display.set_mode(size)
 
-
 pos=(0,0)
 blue = (100,100,200)
 red = (200,100,100)
@@ -23,6 +22,7 @@ screen.fill(blue)
 pygame.font.init() # you have to call this at the start, 
                    # if you want to use this module.
 myfont = pygame.font.SysFont('Comic Sans MS', 10)
+board = pd.DataFrame(data = 0, index = range(1,7) , columns = range(1,8))
 
 def draw_circle(pos,color,r=20):
     x, y = pos
@@ -33,21 +33,24 @@ def add_piece(row, column, player):
     color = colorMap[player]
     draw_circle((column * 60, row * 60), color)
 
-board = pd.DataFrame(data = 0, index = range(1,7) , columns = range(1,8))
+
 
 def draw_board(board):
     for i in board.index:
         for j in board.columns:
             add_piece(i,j,0)
+
 def find_column(pos):
     x,y = pos
     return round(x/60)
-def find_row(column):
+
+def find_row(column, board):
     ret = 6
     for i in board.index:
         if(board.loc[i,column]>0):
             return i-1
     return ret
+
 def check_helper(board, player, row, column, count, dir):
     x , y = dir
     newRow = row + x
@@ -59,11 +62,18 @@ def check_helper(board, player, row, column, count, dir):
     if(board.loc[newRow, newColumn] != player):
         return False
     if(count==1):
-        print(player, newRow, newColumn, dir, sep=" ")
+        #print(player, newRow, newColumn, dir, sep=" ")
         return True
     return check_helper(board, player, newRow, newColumn, count-1, dir)
+def check_tie(board):
+    for column in board.columns:
+        if(board.loc[1,column]==0):
+            return False
+    return True
 
 def check_win(board,player):
+    if(check_tie(board)):
+        return False
     for row in board.index:
         for column in board.columns:
             if(board.loc[row, column]==player):
@@ -74,29 +84,127 @@ def check_win(board,player):
                             if(check_helper(board,player, row,column, 3, dir)):
                                 return True
     return False
+def print_board(board):
+    for row in board.index:
+        val = ""
+        for col in board.columns:
+            val = val + str(board.loc[row, col]) + " "
+        print(val)
 
+def move_helper_check(board,player,column, calls):
+    state = board.copy()
+    row = find_row(column, state)
+    state.loc[row,column]=player
+    if(check_tie(state)):
+        print("Tie Reached")
+        return 0.0
+    if(check_win(state,player)):
+        #print_board(state)
+        #print(player, "wins in" , calls, sep=" ")
+        return (player - 1.5) * 2 / calls
+    return move_helper_add(state, 3 - player , calls+1)
+
+def move_helper_add(board, player, calls):
+    if(calls > 2):
+        return -1
+    state = board.copy()
+    max = -1
+    min = 1
+    ret = (player - 1.5) * -2
+    for col in state.columns:
+        row = find_row(col,state)
+        if(row > 0):
+            temp = move_helper_check(state, player, col, calls)
+            if(player==2):
+                if(temp < min):
+                    min = temp
+                    ret = min
+            else:
+                if(temp > max):
+                    max = temp
+                    ret = max
+    return ret
+
+def get_move(board):
+    state = board.copy()
+    max = -1
+    maxCol = random.randint(1,7)
+    for col in state.columns:
+        row = find_row(col, state)
+        if(row > 0):
+            temp = move_helper_check(state, 2, col, 1)
+            print(temp, col, sep=" ")
+            if(temp > max):
+                maxCol = col
+                max = temp
+    return maxCol
+
+
+class button:
+    def __init__(self,pos,name,color,screen,size=(80,50)):
+        self.tile=pygame.Rect(pos,size)
+        pygame.draw.rect(screen,color,self.tile)
+        pygame.draw.rect(screen,black,self.tile,3)
+        textsurface = myfont.render(name,False,black)
+        x,y=self.tile.center
+        x=x-len(name)*2.5
+        screen.blit(textsurface,(x,y))
+    def has(self,pos):
+        return self.tile.collidepoint(pos)
+
+player_1=button((0,0),"1 Player",blue,screen,(300,480))
+player_2=button((300,0),"2 Player",red,screen,(300,480))
+#screen.fill(white)
+numPlayers=0
+
+while numPlayers==0:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos=pygame.mouse.get_pos()
+            if player_1.has(pos):
+                numPlayers=1
+            if player_2.has(pos):
+                numPlayers=2
+    pygame.display.flip()
+
+screen.fill(blue)
 draw_board(board)
 
 player=1
 win = False
 while not win:
-    for event in pygame.event.get():
         pygame.display.flip()
-        if event.type == pygame.QUIT: 
-            sys.exit()
-        if event.type == pygame.MOUSEBUTTONUP:
-            pos=pygame.mouse.get_pos()
-            column = find_column(pos)
-            if column > 7: break
-            row = find_row(column)
-            #print(column)
-            if row < 1: break
-            #print(find_row(column))
-            board.loc[row,column] = player
-            add_piece(row, column, player)
-            win = check_win(board, player)
-            #if(win): print("you win")
-            player = 3 - player
+        for event in pygame.event.get():
+            pygame.display.flip()
+            if event.type == pygame.QUIT: 
+                sys.exit()
+            if(player==1 or numPlayers == 2):
+                if event.type == pygame.MOUSEBUTTONUP:
+                    pos=pygame.mouse.get_pos()
+                    column = find_column(pos)
+                    if column > 7: break
+                    row = find_row(column, board)
+                    #print(column)
+                    if row < 1: break
+                    #print(find_row(column))
+                    board.loc[row,column] = player
+                    add_piece(row, column, player)
+                    win = check_win(board, player)
+                    #if(win): print("you win")
+                    player = 3 - player
+                    pygame.display.flip()
+            else:
+                row = -1
+                while(row < 1):
+                    column = get_move(board)
+                    row = find_row(column, board)
+                board.loc[row,column] = player
+                add_piece(row, column, player)
+                win = check_win(board, player)
+                player = 3 - player
+                pygame.display.flip()
         pygame.display.flip()
             
 
