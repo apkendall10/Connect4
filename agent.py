288768065a2259df5b0a99ed642526bf6cbd7759
Amos Kendall <apkendall@hornpipe.cs.wisc.edu>
2019-10-02 10:54:49 -0500

    Switched off verbose

--- agent.py
+++ agent.py
@@ -7,14 +7,12 @@ class agent:
         self.game_size = size
         self.ml = (type == 1)
         if(self.ml):
-            self.nn = clf = MLPRegressor(activation='relu', alpha=1e-05, batch_size='auto',
-                    beta_1=0.9, beta_2=0.999, early_stopping=False,
-                    epsilon=1e-08, hidden_layer_sizes=(15,),
-                    learning_rate='constant', learning_rate_init=0.001,
-                    max_iter=200, momentum=0.9, n_iter_no_change=10,
-                    nesterovs_momentum=True, power_t=0.5,  random_state=1,
-                    shuffle=True, solver='lbfgs', tol=0.0001,
-                    validation_fraction=0.1, verbose=False, warm_start=False)
+            self.nn  = MLPRegressor(activation='relu', early_stopping=True, hidden_layer_sizes=(42,7),
+                            learning_rate='adaptive', 
+                            max_iter=200, momentum=0.9, n_iter_no_change=10,
+                            nesterovs_momentum=True, power_t=0.5,  random_state=1,
+                            solver='adam', tol=0.0001,
+                            validation_fraction=0.1, verbose=False, warm_start=False)
             self.train()
 
     def train(self):
